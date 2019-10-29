9c936bf5477e90b05a770dc99d7c9fc12ac363d2
g16918kk <g16918kk@gm.tsuda.ac.jp>
2019-10-29 16:49:00 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -21,16 +21,12 @@ def post_new(request):
     if request.method == "POST": 
         form = PostForm(request.POST)
         m = MeCab.Tagger ("-Ochasen")
-        x=m.parse(form)
-      #if form.is_valid():                                     
+        x=m.parse(form)                               
              post = x.save(commit=False)
              post.author = request.user
              post.published_date = timezone.now()
              post.save()
              return redirect('post_detail', pk=post.pk)                       
-   # else:
-        #form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
