47a38eb91338004bfb5525ed44abdcf56f9a5364
g16918kk <g16918kk@gm.tsuda.ac.jp>
2019-10-29 17:28:17 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -27,7 +27,7 @@ def post_new(request):
         post.save()
         return redirect('post_detail', pk=post.pk)
     else:
-            form=PostForm()
+        form=PostForm()
             return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
