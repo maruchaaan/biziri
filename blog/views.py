bc62479a20cb7135ad4695ec1f6a266a05224acd
g16918kk <g16918kk@gm.tsuda.ac.jp>
2019-10-29 14:16:42 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ def post_detail(request, pk):
 def post_new(request):
     if request.method == "POST": 
         form = PostForm(request.POST)
-         if form.is_valid():
+        if form.is_valid():                                     
              post = form.save(commit=False)
              post.author = request.user
              post.published_date = timezone.now()
