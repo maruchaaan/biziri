46914da79627290375c7ab91b1018e0f525ddc8b
g16918kk <g16918kk@gm.tsuda.ac.jp>
2019-10-30 12:18:14 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,3 @@
-# coding: utf-8
-import sys
-sys.path.append('../blog/myvenv/lib/python3.7/site-packages')
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
