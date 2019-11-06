e51b811a13dc9880a8e14bf4cd0607b86cb2c073
g16918kk <g16918kk@gm.tsuda.ac.jp>
2019-11-06 15:55:43 +0900

    Added views to create/edit blog post inside the site.

---
+++ blog/templates/blog/kaiseki.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+import sys
+import MeCab
+import cgi
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+form = cgi.FieldStorage()
+if 'sentense' not in form:
+    print('Content-type: text/html; charset=UTF-8')
+    print('')
+    print('sentense フィールドが送信されていません。')
+    sys.exit()
+
+print('Content-type: text/html; charset=UTF-8')
+print('')
+print(your_name) 
