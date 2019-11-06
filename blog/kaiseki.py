9b3be2f024c059249b4e55caa5053b819b11d6ba
g16918kk <g16918kk@gm.tsuda.ac.jp>
2019-11-06 16:33:37 +0900

    Added views to create/edit blog post inside the site.

--- blog/kaiseki.py
+++ blog/kaiseki.py
@@ -4,6 +4,8 @@ import MeCab
 import cgi
 import io
 
+def result(request):
+    return render(request, 'blog/hoge3.html')
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 form = cgi.FieldStorage()
 if 'sentense' not in form:
@@ -14,4 +16,4 @@ if 'sentense' not in form:
 
 print('Content-type: text/html; charset=UTF-8')
 print('')
-print(your_name) 
+print(sentense) 
