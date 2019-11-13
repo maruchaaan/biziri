ccfa06db9e9b8406bb33c091b042a1e547a6ace9
g16918kk <g16918kk@gm.tsuda.ac.jp>
2019-11-13 21:36:35 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
-from djangogirls import MeCab
+from Python.MeCab import MeCab
 
 
 def post_list(request):
