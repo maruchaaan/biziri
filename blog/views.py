f8514d309c6ebd0c739f27030feb2caa0aaf22ca
g16918kk <g16918kk@gm.tsuda.ac.jp>
2019-11-14 10:25:13 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -72,7 +72,7 @@ def result2(request):
     d={
         'comment2': request.GET.get('comment2')
     }
-    mt = MeCab.Tagger("-Ochasen")
+    mt = MeCab("-Owakati")
     str_in=d
     res = mt.parseToNode(str_in)
     dousi=[]
